c0f839a73305cba3a57154d1e5a16a4eb7e6bd37
Tommy PAGEARD <tommy.pageard@student.42.fr>
2017-04-01 16:42:26 +0200

    get disk info for now : free used total

--- agent/services/disk_info.go
+++ agent/services/disk_info.go
@@ -1,10 +1,41 @@
 package services
 
 import (
+	"fmt"
 	"net"
+	"syscall"
+)
+
+type DiskStatus struct {
+	All  uint64 `json:"all"`
+	Used uint64 `json:"used"`
+	Free uint64 `json:"free"`
+}
+
+const (
+	B  = 1
+	KB = 1024 * B
+	MB = 1024 * KB
+	GB = 1024 * MB
 )
 
 func Disk_Info(conn net.Conn) {
-	res := []byte("disk\n")
-	_, _ = conn.Write(res)
+	// res := []byte("disk\n")
+	// _, _ = conn.Write(res)
+
+	var disk DiskStatus
+
+	fs := syscall.Statfs_t{}
+	err := syscall.Statfs("/", &fs)
+	if err != nil {
+		return
+	}
+
+	disk.All = fs.Blocks * uint64(fs.Bsize)
+	disk.Free = fs.Bfree * uint64(fs.Bsize)
+	disk.Used = disk.All - disk.Free
+
+	fmt.Printf("All: %.2f GB\n", float64(disk.All)/float64(GB))
+	fmt.Printf("Used: %.2f GB\n", float64(disk.Used)/float64(GB))
+	fmt.Printf("Free: %.2f GB\n", float64(disk.Free)/float64(GB))
 }
